cca5e16853ec91b2508f474e2d5cfbc4df2b7023
Rick Shory <rickshory@gmail.com>
2019-06-05 22:09:19 -0700

    5->6 bug fix

--- app/src/main/java/com/rickshory/diceroller/MainActivity.kt
+++ app/src/main/java/com/rickshory/diceroller/MainActivity.kt
@@ -35,7 +35,7 @@ class MainActivity : AppCompatActivity() {
             3 -> R.drawable.dice_3
             4 -> R.drawable.dice_4
             5 -> R.drawable.dice_5
-            6 -> R.drawable.dice_5
+            6 -> R.drawable.dice_6
             else -> R.drawable.empty_dice
         }
         diceImage.setImageResource(drawableResource)
