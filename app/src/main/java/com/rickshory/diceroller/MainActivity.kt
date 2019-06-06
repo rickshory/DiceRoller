abe8d905e6c842faf3152acefb289bfb87168274
Rick Shory <rickshory@gmail.com>
2019-06-05 18:52:26 -0700

    test update text

--- app/src/main/java/com/rickshory/diceroller/MainActivity.kt
+++ app/src/main/java/com/rickshory/diceroller/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
 
@@ -22,5 +23,8 @@ class MainActivity : AppCompatActivity() {
 
     private fun rollDice() {
         val resultText = findViewById<TextView>(R.id.result_text)
+//        val randomInt = Random().nextInt(6) + 1
+//        resultText.text = randomInt.toString()
+        resultText.text = "R O L L E D"
     }
 }
